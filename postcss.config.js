99cb2c66ca1203c3cbf77c351f9f4f19e554a7db
Alyssa Denary <alyssa.denary@gmail.com>
2023-02-11 16:09:18 -0500

    refactor: update files to postcss

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require("postcss-import"),
     require("postcss-preset-env")({ stage: 1 }),
-    require("cssnano"),
+    // require("cssnano"),
     require("postcss-nested"),
     require("postcss-custom-media"),
     require("postcss-media-minmax"),
