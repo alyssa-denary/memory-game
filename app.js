fb97e5e978dc68bd94bdac1b647fc083b90eb019
Alyssa Denary <alyssa.denary@gmail.com>
2023-02-13 15:34:26 -0500

    update: deck easy

--- app.js
+++ app.js
@@ -7,16 +7,12 @@ const COLORS = [
   "red",
   "blue",
   "green",
-  "orange",
   "purple",
   "red",
   "blue",
   "green",
-  "orange",
   "purple",
 ];
-
-// startGame(COLORS, FOUND_MATCH_WAIT_MSECS);
 
 // add event listener to start button
 document.getElementById("start-button").addEventListener("click", (e) => {
