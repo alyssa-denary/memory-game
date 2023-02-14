4d0cf78a3ef268ab741e86c5eb3ab1d915ce88f3
Alyssa Denary <alyssa.denary@gmail.com>
2023-02-14 12:08:00 -0500

    add: larger bank of colors for cards

--- app.js
+++ app.js
@@ -3,11 +3,33 @@
 /** Memory game: find matching pairs of cards and flip both of them. */
 
 const NO_MATCH_DISPLAY_TIME = 1000;
-const COLORS = ["red", "blue", "green", "purple"];
+const COLORS = [
+  "red",
+  "orange",
+  "yellow",
+  "blue",
+  "green",
+  "purple",
+  "brown",
+  "black",
+  "grey",
+  "skyblue",
+  "maroon",
+  "fuchsia",
+  "lime",
+  "olive",
+  "teal",
+  "aqua",
+  "navy",
+  "aliceblue",
+  "aquamarine",
+  "antiquewhite",
+  "blueviolet",
+  "darkslateblue",
+  "gold",
+  "indigo",
+];
 
 document.getElementById("best-score").textContent = findBestScore();
 
-document.getElementById("start-button").addEventListener("click", (e) => {
-  startGame(COLORS, NO_MATCH_DISPLAY_TIME);
-  document.getElementById("landing-page").style.visibility = "hidden";
-});
+
