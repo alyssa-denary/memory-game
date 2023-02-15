af30b6107f7c675974b43a58621aa262c38fe6d4
Alyssa Denary <alyssa.denary@gmail.com>
2023-02-15 18:35:44 -0500

    refactor: display time, remove image array

--- app.js
+++ app.js
@@ -2,33 +2,7 @@
 
 /** Memory game: find matching pairs of cards and flip both of them. */
 
-const NO_MATCH_DISPLAY_TIME = 1000;
-const COLORS = [
-  "red",
-  "orange",
-  "yellow",
-  "blue",
-  "green",
-  "purple",
-  "brown",
-  "black",
-  "grey",
-  "skyblue",
-  "maroon",
-  "fuchsia",
-  "lime",
-  "olive",
-  "teal",
-  "aqua",
-  "navy",
-  "aliceblue",
-  "aquamarine",
-  "antiquewhite",
-  "blueviolet",
-  "darkslateblue",
-  "gold",
-  "indigo",
-];
+const NO_MATCH_DISPLAY_TIME = 3000;
 
 const IMAGES = [
   "img/cards/air-capture.jpg",
