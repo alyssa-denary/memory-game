e30d9fd6fc48147d37d8b6196a6a219cc5980689
Alyssa Denary <alyssa.denary@gmail.com>
2023-02-16 14:46:06 -0500

    refactor: game difficulty added within fx

--- app.js
+++ app.js
@@ -38,7 +38,6 @@ document.getElementById("game-form").addEventListener("submit", (e) => {
   const difficulty = document.querySelector(
     'input[name="difficulty"]:checked'
   ).value;
-  document.getElementById("game").className = difficulty;
   startGame(IMAGES, NO_MATCH_DISPLAY_TIME, difficulty);
   document.getElementById("landing-page").style.visibility = "hidden";
 });
